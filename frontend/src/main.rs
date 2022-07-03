c576923239f70cc289215fe96a1c944e3e45bb43
Borys Tyran <bt@btmb.local>
2022-07-03 21:04:22 +0200

    feat: make frontend display yew tutorial

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,46 +1,48 @@
 use yew::prelude::*;
+use serde::Deserialize;
+use reqwasm::http::Request;
 
-enum Msg {
-    AddOne,
+#[derive(Clone, PartialEq, Deserialize)]
+struct Measurement {
+    id: usize,
+    title: String,
+    speaker: String,
+    url: String,
 }
 
-struct Model {
-    value: i64,
-}
+#[function_component(App)]
+fn app() -> Html {
 
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
+    let measurements: UseStateHandle<Vec<Measurement>> = use_state(|| vec![]);
+    {
+        let measurements = measurements.clone();
+        use_effect_with_deps(move |_| {
+            let measurements = measurements.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+            let fetched_measurements: Vec<Measurement> = Request::get("/tutorial/data.json")
+               .send()
+               .await
+               .unwrap()
+               .json()
+               .await
+               .unwrap();
+                measurements.set(fetched_measurements);
+            });
+            || ()
+        }, ())
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
+    html! {
+        <>
+        <h1>{ "Hello World" }</h1>
+        <div>
+            <p>{ format!("Is empty: {}", measurements.is_empty()) }</p>
+            <p>{ format!("Length of vector: {}", measurements.len()) }</p>
+        </div>
+        </>
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<App>();
 }
